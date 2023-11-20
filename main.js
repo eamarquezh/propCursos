506e4cca752d4446e78ae89b07fcd1092866b227
eamarquezh <supericman@outlook.com>
2023-11-19 21:07:21 -0600

    modificaciones

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ slideContenedor.appendChild(slideSubItem4);
 
 // Funciónes
 function abrirChatWhatsApp() {
-  window.open('https://wa.me/7711208704/?text=solicitud%20Informes', '_blank');
+  window.open('https://wa.me/6612923092/?text=solicitud%20Informes', '_blank');
 }
 
 function createCard(title, imageSrc, price, contactLink, description) {
@@ -77,9 +77,9 @@ function createCard(title, imageSrc, price, contactLink, description) {
         // Ejemplo de uso
         var bodyCard1 = document.createElement('div');
         bodyCard1.className="bodyCard";
-        var card1 = createCard('CERTIFICACION 1 EC0217.01', 'curso1.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/7711208704/?text=Informes%20curso1', 'Imparticion de cursos de formación de capital humano de manera presencial grupal.');
-        var card2 = createCard('CERTIFICACIÓN 2 EC0301', 'curso2.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/7711208704/?text=Informes%20curso2', 'Diseño de cursos de formación del capital humano de manera presencial grupal, sus instrumentos de evaluación y manuales del curso.');
-        var card3 = createCard('CERTIFICACIÓN 3 EC0091', 'curso3.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/7711208704/?text=Informes%20curso3', 'Verificación externa de la operación de los centros de evaluación y evaluadores independientes.');
+        var card1 = createCard('CERTIFICACION 1 EC0217.01', 'curso1.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/6612923092/?text=Informes%20curso1', 'Imparticion de cursos de formación de capital humano de manera presencial grupal.');
+        var card2 = createCard('CERTIFICACIÓN 2 EC0301', 'curso2.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/6612923092/?text=Informes%20curso2', 'Diseño de cursos de formación del capital humano de manera presencial grupal, sus instrumentos de evaluación y manuales del curso.');
+        var card3 = createCard('CERTIFICACIÓN 3 EC0091', 'curso3.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/6612923092/?text=Informes%20curso3', 'Verificación externa de la operación de los centros de evaluación y evaluadores independientes.');
         // Agregar las tarjetas al cuerpo del documento
         bodyCard1.appendChild(card1);
         bodyCard1.appendChild(card2);
@@ -89,8 +89,8 @@ function createCard(title, imageSrc, price, contactLink, description) {
         var bodyCard2 = document.createElement('div');
         bodyCard2.className="bodyCard";
         
-        var card4 = createCard('CERTIFICACIÓN 4 EC0076', 'curso4.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/7711208704/?text=Informes%20curso4', 'Evaluación de la competencia de candidatos con base en Estándares de Competencia');
-        var card5 = createCard('CERTIFICACIÓN 5 EC0305', 'curso5.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/7711208704/?text=Informes%20curso5', 'Prestación de servicios de atención a clientes');
+        var card4 = createCard('CERTIFICACIÓN 4 EC0076', 'curso4.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/6612923092/?text=Informes%20curso4', 'Evaluación de la competencia de candidatos con base en Estándares de Competencia');
+        var card5 = createCard('CERTIFICACIÓN 5 EC0305', 'curso5.jpg', 'De:<s>$2,500</s> a $1,800 (tiempo limitado)', 'https://wa.me/6612923092/?text=Informes%20curso5', 'Prestación de servicios de atención a clientes');
         // Agregar las tarjetas al cuerpo del documento
         bodyCard2.appendChild(card4);
         bodyCard2.appendChild(card5);
